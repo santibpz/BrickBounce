b78ac0de3d16fa7fee9f9f739d0f2c95524967f9
IanVazquez <A01027225@tec.mx>
2023-04-28 14:52:05 -0600

    add sounds

--- BrickBounceGame/Assets/Scripts/Bouncycontroll.cs
+++ BrickBounceGame/Assets/Scripts/Bouncycontroll.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Bouncycontroll : MonoBehaviour
 {
+    AudioSource audioSource;
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject)
+        {
+            GetComponent<AudioSource>().Play();
+        }
         if (collision.gameObject.CompareTag("plataform"))
         {
             rb.gravityScale = 0f;
