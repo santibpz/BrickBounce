b42dbaa80f4334c56e9d4b1f0acd506f73cf00d0
Santiago Benitez <A01782813@tec.mx>
2023-04-28 22:53:56 -0600

    added initial block logic

--- BrickBounceGame/Assets/Scripts/BouncyControl.cs
+++ BrickBounceGame/Assets/Scripts/BouncyControl.cs
@@ -1,14 +1,21 @@
+// script for controlling the ball movement
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bouncycontroll : MonoBehaviour
+public class BouncyControl : MonoBehaviour
 {
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
