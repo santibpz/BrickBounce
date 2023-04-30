71a44b2c7522e72eb7da4e9ca0c6c861158ddee4
Santiago Benitez <A01782813@tec.mx>
2023-04-29 22:56:44 -0600

    added score functionality

---
+++ BrickBounceGame/Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+// script for controlling the score logic of the game
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+
+    [SerializeField] TMP_Text scoreText;
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Block"))
+        {
+            score += 1;
+            scoreText.text = "Score: " + score;
+        } 
+    }
+}
